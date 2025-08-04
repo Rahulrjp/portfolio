611368320f3b55fcaf52d08f96dec505d8abb19d
Rahul Vishwakarma <rahulrjp16@gmail.com>
2025-08-04 21:36:06 +0530

    cors fixed

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 import { sendContactForm } from "./controllers/contact.controller.js"
 
 const app = express()
-app.use(cors({origin : 'http://localhost:5173' , credentials : true}))
+app.use(cors({origin : 'https://portfolio-ihaj.onrender.com' , credentials : true}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 // app.use(session({ secret: "my-secret", resave: true, saveUninitialized: false }))
